a26c457796f5345e95049611ba37310e4e58f099
shawnzhang12 <xiaoen.zhang@mail.utoronto.ca>
2019-12-31 23:06:03 -0500

    mesh temperature code

--- uno_temp/uno_temp.ino
+++ uno_temp/uno_temp.ino
@@ -1,12 +1,4 @@
 
-/** RF24Mesh_Example.ino by TMRh20
-   This example sketch shows how to manually configure a node via RF24Mesh, and send data to the
-   master node.
-   The nodes will refresh their network address as soon as a single write fails. This allows the
-   nodes to change position in relation to each other and the master node.
-*/
-
-
 #include "RF24.h"
 #include "RF24Network.h"
 #include "RF24Mesh.h"
@@ -23,13 +15,6 @@ RF24 radio(7, 8);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 
-/**
-   User Configuration: nodeID - A unique identifier for each radio. Allows addressing
-   to change dynamically with physical changes to the mesh.
-   In this example, configuration takes place below, prior to uploading the sketch to the device
-   A unique value from 1-255 must be configured for each node.
-   This will be stored in EEPROM on AVR devices, so remains persistent between further uploads, loss of power, etc.
- **/
 #define nodeID 1
 
 uint32_t displayTimer = 0;
